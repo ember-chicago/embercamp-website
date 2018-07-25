274413342249f8d24bf71abf6bfc14a00a7a3d69
howie <db@redshelf.com>
2018-07-25 08:48:29 -0500

    Adds 201 created sponsor info

--- app/components/sponsors-list.js
+++ app/components/sponsors-list.js
@@ -1,5 +1,10 @@
 import Component from '@ember/component';
+import { TIER_ONE_SPONSORS, TIER_THREE_SPONSORS, TIER_TWO_SPONSORS } from "../constants/sponsors";
 
 export default Component.extend({
+  platinum: TIER_ONE_SPONSORS,
+  gold: TIER_TWO_SPONSORS,
+  bronze:TIER_THREE_SPONSORS,
+
   classNames: ['sponsors-list']
 });
