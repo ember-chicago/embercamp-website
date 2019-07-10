2c961ab364564ae42bbae86058aebd8ea8007a86
Melanie Sumner <melaniersumner@gmail.com>
2019-07-10 10:38:44 -0500

    updated speaker info

--- app/constants/speakers.js
+++ app/constants/speakers.js
@@ -1,8 +1,8 @@
 export const SPEAKERS = [
   {
     name: 'Sean Devine ',
-    twitterHandle: '',
-    avatar: null,
+    twitterHandle: 'barelyknown',
+    avatar: '/assets/images/speakers/sean-devine.jpg',
     bio: `Sean Devine is the proud father of 5 children, a lucky husband to his amazing wife Theresa, an active Ember/JavaScript (and Rails/Ruby) developer, and the Founder and CEO of XBE - a Construction Logistics business based in Chicago, IL. He's thankful every day that he bet his company on Ember, and is more excited than ever about where things headed. When not working, or parenting, or programming, Sean enjoys running, cycling, and the NBA.`,
     label: '',
     type: 'speaker',
@@ -11,8 +11,8 @@ export const SPEAKERS = [
   },
   {
     name: 'Edward Faulkner',
-    twitterHandle: '',
-    avatar: null,
+    twitterHandle: 'eaf4',
+    avatar: '/assets/images/speakers/ed-faulkner.jpeg',
     bio: 'Edward Faulkner is a member of the Ember Framework core team. His open source code is running on mainstream gaming consoles, major social media sites, and hordes of enterprise applications. His consultancy, Polynomial LLC, leads ambitious software projects for a diverse group of businesses and nonprofits, and he is a lead developer for the Cardstack Project. https://cardstack.com ',
     label: '',
     type: 'speaker',
@@ -21,8 +21,8 @@ export const SPEAKERS = [
   },
   {
     name: 'Dan Gebhardt',
-    twitterHandle: '',
-    avatar: null,
+    twitterHandle: 'dgeb',
+    avatar: '/assets/images/speakers/dan-gebhardt.jpeg',
     bio: 'Dan is on the Ember.js core team, an editor of the JSON:API spec, and the creator of Orbit.js. He consults through Cerebris, the company he co-founded, and their partner Tilde. He loves to travel and hike with his family and fluffy dog.',
     label: '',
     type: 'speaker',
@@ -31,8 +31,8 @@ export const SPEAKERS = [
   },
   {
     name: 'Marie Chatfield',
-    twitterHandle: '',
-    avatar: null,
+    twitterHandle: 'mariechatfield',
+    avatar: '/assets/images/speakers/marie-chatfield.jpg',
     bio: 'Marie Chatfield writes code and poetry, sometimes at the same time. As a front-end engineering enthusiast, she’s currently helping the fine folks at Pingboard build the world’s best org chart software. She is passionate about creating inclusive experiences and understanding foundational web technologies at a deeper level. Talk to her about rock climbing or Texas!',
     label: '',
     type: 'speaker',
@@ -42,7 +42,7 @@ export const SPEAKERS = [
   {
     name: ' Lisa Backer',
     twitterHandle: '',
-    avatar: null,
+    avatar: '/assets/images/speakers/lisa-backer.jpg',
     bio: 'Lisa is a senior engineer at DockYard, helping large clients integrate Ember.js at scale with a commitment to quality. This commitment has led to a passion for testing. She has 20 years of web development experience ranging from consulting and product development, to helping nonprofit institutions in both paid and volunteer capacities.',
     label: '',
     type: 'speaker',
@@ -52,7 +52,7 @@ export const SPEAKERS = [
   {
     name: ' Matt Gardner',
     twitterHandle: '',
-    avatar: null,
+    avatar: '/assets/images/speakers/matt-gardner.jpg',
     bio: `Matt Gardner is a software engineer for the NYC Department of City Planning. Originally an urban planner, he pivoted into application development after seeing a gap in need for better software in government, specifically in the public advocacy realm. He and his team, Planning Labs, rebuilt the City of New York's digital zoning and land use map using EmberJS.`,
     label: '',
     type: 'speaker',
@@ -61,8 +61,8 @@ export const SPEAKERS = [
   },
   {
     name: 'James Steinbach',
-    twitterHandle: '',
-    avatar: null,
+    twitterHandle: 'jdsteinbach',
+    avatar: '/assets/images/speakers/james-steinbach.png',
     bio: `James is a senior developer with more than 10 years of front-end experience. If you let him talk, you're liable to get an earful about modular styles, accessible pages, cool CSS tricks, or random restaurant reviews (especially if pizza or BBQ are involved).`,
     label: '',
     type: 'speaker',
@@ -70,9 +70,9 @@ export const SPEAKERS = [
     id: 7
   },
   {
-    name: 'Spencer P',
+    name: 'Spencer Price',
     twitterHandle: '',
-    avatar: null,
+    avatar: '/assets/images/speakers/spencer-price.jpg',
     bio: `Spencer is an Engineering Manager for Movable Ink in New York City. He has been building applications with Ember for over seven years and has been experimenting with use cases for modifiers ever since they were formally announced in February. Outside of work, you'll find me blocking people's view at Broadway shows, walking my obnoxiously happy dog, or watching good movies.`,
     label: '',
     type: 'speaker',
@@ -81,8 +81,8 @@ export const SPEAKERS = [
   },
   {
     name: 'Jen Weber',
-    twitterHandle: '',
-    avatar: null,
+    twitterHandle: 'jwwwweber',
+    avatar: '/assets/images/speakers/jen-weber.jpg',
     bio: 'Jen Weber is a member of the Ember Core Team. She works at Cardstack, making Web 3.0 accessible to everyone and empowering new creators. (she/her/they/them)',
     label: '',
     type: 'speaker',
@@ -92,11 +92,21 @@ export const SPEAKERS = [
   {
     name: 'Preston Sego',
     twitterHandle: '',
-    avatar: null,
+    avatar: '/assets/images/speakers/preston-sego.jpg',
     bio: 'Preston Sego is an Open Source Enthusiast. He has a side project, emberclear.io, which is both a place to explore new features of the web platform, and help others learn Ember and frontend testing patterns. He works at CrowdStrike, helping build interfaces for the cloud-native endpoint protection platform built to stop breaches.',
     label: '',
     type: 'speaker',
     talkTitle: 'May I ask a question, LIVE!',
     id: 10
+  },
+  {
+    name: 'Olivia Liddell',
+    twitterHandle: 'oliravi',
+    avatar: '/assets/images/speakers/olivia-liddell.jpg',
+    bio: 'Olivia Liddell is a Cloud Training Specialist at Cloudbakers. She is a former Chicago Public Schools teacher who now specializes in technology training and change management. She is passionate about teaching, mentoring, and distance running.',
+    label: '',
+    type: 'keynote',
+    talkTitle: 'Closing Keynote Speaker',
+    id: 11
   }
 ];
